69a8c943548849024d7da44a18e72dc4257b57d5
David Michael <david.michael@meredith.com>
2016-10-04 13:16:28 -0500

    removing dry run for testing

--- parsync.py
+++ parsync.py
@@ -20,7 +20,7 @@ for subdirectory in subdirectories:
 def grab_data_from_queue():
     while not q.empty():
         sub_dir = q.get()
-        call(['rsync', '-rpg','--dry-run', '%s/%s' % (src_dir, sub_dir), '%s' % dest_dir])
+        call(['rsync', '-rpg', '%s/%s' % (src_dir, sub_dir), '%s' % dest_dir])
         print('done with %s' % sub_dir)
         q.task_done()
 
